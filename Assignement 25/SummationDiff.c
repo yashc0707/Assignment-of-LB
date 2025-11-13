2b6c52bc4b02e4b72ab829b0c0d33dfe0cfeec0f
Yashraj Chavan <134805049+yashc0707@users.noreply.github.com>
2025-11-14 03:21:19 +0530

    Add files via upload

---
+++ Assignement 25/SummationDiff.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int Difference(int Arr[],int iSize)
+{
+    int iCnt=0,iEven=0,iOdd=0;
+
+    for(iCnt=0;iCnt<iSize;iCnt++)
+    {
+        if(Arr[iCnt]%2==0)
+        {
+            iEven=iEven+Arr[iCnt];
+        }
+        else
+        {
+            iOdd=iOdd+Arr[iCnt];
+        }
+    }
+    return (iEven-iOdd);
+}
+int main()
+{
+    int iLength=0;
+    int *Brr=NULL;
+    int iCnt=0;
+    int iRet=0;
+
+    printf("Enter the Number of Array:");
+    scanf("%d",&iLength);
+
+    Brr=(int*) malloc(iLength * sizeof(int));
+
+    if(NULL == Brr)
+    {
+        printf("Unable to allocate memory");
+        return -1;
+    }
+
+    printf("Enter the Element:");
+
+    for(iCnt=0;iCnt<iLength;iCnt++)
+    {
+        scanf("%d",&Brr[iCnt]);
+    }
+
+    iRet=Difference(Brr,iLength);
+    printf("Count of Even is:%d\n",iRet);
+
+}
