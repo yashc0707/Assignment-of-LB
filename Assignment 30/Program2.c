db7f83ef229bb903c9e2ce560ed7b9b11abac9f2
Yashraj Chavan <134805049+yashc0707@users.noreply.github.com>
2025-11-21 04:42:21 +0530

    Add files via upload

---
+++ Assignment 30/Program2.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+void Display(int iRow,int iCol)
+{
+    int i=0,j=0;
+   
+
+    for(i=1;i<=iRow;i++)
+    {
+        for(j=iCol;j>=1;j--)
+        {
+            while(j>=i)
+            {
+                printf("*\t");
+                j--;
+            }
+            if(j<1)
+            {
+                break;
+            } 
+            printf("#\t");
+
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1=0,iValue2=0;
+
+    printf("Enter the number of Rows:");
+    scanf("%d",&iValue1);
+
+    printf("Enter the number of Cloumn:");
+    scanf("%d",&iValue2);
+
+    Display(iValue1,iValue2);
+}
